24f0e4558d64fee0ea3a55ec971fdc993a72cd8e
roberto vera <robertodevstack@gmail.com>
2023-12-17 14:58:55 -0300

    camb

--- query.js
+++ query.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
 host: 'localhost',
 user: 'postgres',
 password: 'postgres',
- database: 'Registro_ul3l', 
+ database: 'farmacia', 
  allowExitOnIdle: true
 })
 
